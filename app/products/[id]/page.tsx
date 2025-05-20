1801f025adedcc9089926f15e0a950a8499d0b49
Gopikalakshmi <gopikalakshmia@gmail.com>
2025-05-20 11:51:00 -0400

    product list and product id page added

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,25 @@
 
+import { productList } from "@/app/productList"
+import Image from "next/image";
+import { notFound } from "next/navigation";
 interface productParms{
 params:{id:string};
 }
 export default function ProductPage({params}:productParms){
-    return (<h1>{params.id}</h1>)
+    const selectedProduct=productList.find(item=>item.Id===params.id);
+    if(!selectedProduct){
+        return notFound();
+    }
+    return (   <div className="flex flex-col items-center p-6">
+      <Image
+        src={selectedProduct.ImageLink}
+        alt={selectedProduct.Name}
+        width={200}
+        height={200}
+        className="rounded"
+      />
+      <h1 className="text-2xl font-bold mt-4">{selectedProduct.Name}</h1>
+      <p className="text-gray-600 mt-2 text-center">{selectedProduct.Description}</p>
+      <p className="text-lg font-semibold text-blue-700 mt-2">${selectedProduct.Price}</p>
+    </div>)
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,15 @@
+import Image from 'next/image';
+import { productList} from '../productList';
+import Link from 'next/link';
 export default function ProductsListPage(){
+ 
     return(
-        <h1>Products Page</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
+        {productList.map(item=><Link href={`/products/${item.Id}`} key={item.Id} className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center">
+              <Image src={item.ImageLink} alt="Product Image" width={100} height={100} />
+            <p>{item.Description}</p>
+            <h1>${item.Price}</h1>
+        </Link>)}
+        </div>
     )
 }

4e69b9af36feca979916343c2cd5c588dce4217d
Gopikalakshmi <gopikalakshmia@gmail.com>
2025-05-29 17:28:21 -0400

    Added the feature if the product item already existed

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,14 +1,55 @@
-
-
-import { connectToDb } from '../db';
-export async function GET(){
-  const {db}=await connectToDb();
-  const productList=await db.collection('Products').find({}).toArray();
-  return new Response(JSON.stringify(productList),
-  {status:200,
-    headers:{
-        "Content-Type":"application/json"
+import { NextRequest } from "next/server";
+import { connectToDb } from "../db";
+export async function GET() {
+  const { db } = await connectToDb();
+  const productList = await db.collection("Products").find({}).toArray();
+  return new Response(JSON.stringify(productList), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+export async function POST(req: NextRequest) {
+  try {
+    const cart = await req.json();
+    const { db } = await connectToDb();
+    const existingCart = await db
+      .collection("Cart")
+      .findOne({ userId: cart.userId, productId: cart.productId });
+    if (existingCart) {//if the product alread exist,add the qty by 1
+      const result = await db
+        .collection("Cart")
+        .updateOne(
+          { userId: cart.userId, productId: cart.productId },
+          { $inc: { Qty: cart.Qty } }
+        );
+      return new Response(
+        JSON.stringify({
+          message: "The product has been inserted",
+          insertId: result.modifiedCount,
+        }),
+        { status: 201 }
+      );
     }
-  })
-
+    if (!existingCart) {//if the product is not added,insert the new product
+      const result = await db.collection("Cart").insertOne(cart);
+      return new Response(
+        JSON.stringify({
+          message: "The product has been inserted",
+          insertId: result.insertedId,
+        }),
+        { status: 201 }
+      );
+    }
+  } catch (error) {
+    console.error("Insert error:", error);
+    return new Response(
+      JSON.stringify({ message: "Failed to insert product" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
 }
--- app/products/components/productCard.tsx
+++ app/products/components/productCard.tsx
@@ -1,37 +1,47 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import { Product } from '../page';
+import { Product } from "../page";
 
-export default function ProductCard({ productItem }: { productItem: Product }){
-
-  const handleAddToCart = (productId: string) => {
+export default function ProductCard({ productItem }: { productItem: Product }) {
+  const handleAddToCart = async (productId: string) => {
     console.log(productId);
+    const cart = { userId: "1", productId: productId, Qty: 1 };
+    const response = await fetch("http://localhost:3000/api/products", {
+      method: "POST",
+      body: JSON.stringify(cart),
+      headers: { "Content-Type": "application/json" },
+    });
+    if(!response.ok){
+      console.log("Product is not added to the Cart")
+    }else {
+  console.log("Product is  added to the Cart");
+}
   };
 
-
-return(
-<div key={productItem.Id} className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center">
-          <Link
-            href={`/products/${productItem.Id}`}
-          >
-              <div className="text-center cursor-pointer">
-            <Image
-              src={productItem.ImageLink}
-              alt="Product Image"
-              width={100}
-              height={100}
-            />
-            <p>{productItem.Description}</p>
-            <h1>${productItem.Price}</h1>
-            </div>
-          </Link>
-          <button
-            className="bg-blue-600 text-white px-4 py-2 rounded m-5 hover:bg-blue-700"
-            onClick={() => handleAddToCart(productItem.Id)}
-          >
-            Add to Cart
-          </button>
+  return (
+    <div
+      key={productItem.Id}
+      className="bg-white shadow-lg rounded-lg p-4 flex flex-col items-center"
+    >
+      <Link href={`/products/${productItem.Id}`}>
+        <div className="text-center cursor-pointer">
+          <Image
+            src={productItem.ImageLink}
+            alt="Product Image"
+            width={100}
+            height={100}
+          />
+          <p>{productItem.Description}</p>
+          <h1>${productItem.Price}</h1>
         </div>
-)
+      </Link>
+      <button
+        className="bg-blue-600 text-white px-4 py-2 rounded m-5 hover:bg-blue-700"
+        onClick={() => handleAddToCart(productItem.Id)}
+      >
+        Add to Cart
+      </button>
+    </div>
+  );
 }
